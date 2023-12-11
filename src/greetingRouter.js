aa817135c8a97e58f48caa773b44c23be140d98c
cristobalJR <cristobaljusrtz@gmail.com>
2023-12-11 22:27:50 +0100

    Actualización

--- src/greetingRouter.js
+++ src/greetingRouter.js
@@ -12,12 +12,20 @@ router.get('/', (req, res) => {
   res.render('index', data);
 });
 
+router.get('/detalle/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const data = {
+    preguntas: preguntasService.getPreguntaById(id),
+  };
+  res.render('detalle', data);
+});
+
+
 router.get('/respuesta/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const data = {
       preguntas: preguntasService.getPreguntaById(id),
     };
-  
     res.render('respuesta', data);
   });
 
@@ -32,24 +40,25 @@ router.get('/error/:id/:eror_mes', (req, res) => {
   res.render('error404', data);
 });
 
-router.post('/add', (req, res) => {
-  const pregunta = req.body.pregunta;
-  const newPregunta = preguntasService.addPregunta(pregunta);
+router.post('/addComentario', (req, res) => {
+  const ComentarioNuevo = req.body.ComentarioNuevo;
+ 
+  res.send(`-${ComentarioNuevo}-`)
 
   // Puedes redirigir a la página de detalles o hacer algo más aquí
-  res.redirect(`/detalle/${newPregunta.id}`);
+  // res.redirect(`/detalle/${newPregunta.id}`); -${dificultad}-  const dificultad = req.body.dificultad;
 });
+router.post('/addPregunta', (req, res) => {
+  const { pregunta, respuesta, imagenPregunta, imagenRespuesta, tema, dificultad, comentarios } = req.body;
 
-router.post('/update/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const pregunta = req.body.pregunta;
-  const updatedPregunta = preguntasService.updatePregunta(id, pregunta);
-
-  if (updatedPregunta) {
-    res.redirect(`/detalle/${id}`);
-  } else {
-    res.status(404).redirect(`/error/${id}/Elemento no encontrado`);
-  }
+  // Llamar al servicio de preguntas para agregar la pregunta
+  const addPregunta = preguntasService.addPregunta(imagenPregunta, imagenRespuesta, pregunta, respuesta, tema, dificultad, comentarios);
+  if(addPregunta){
+    res.redirect("/")
+  } else{
+    res.status(404).redirect(`/error/nuevaPregunta/No se pudo agregar la nueva pregunta`)  }
+  // Puedes redirigir a la página de detalles o hacer algo más aquí
+  // res.redirect(`/detalle/${newPregunta.id}`); 
 });
 
 router.post('/delete/:id', (req, res) => {
@@ -62,6 +71,31 @@ router.post('/delete/:id', (req, res) => {
   } else {
     res.status(404).redirect(`/error/${id}/Elemento no encontrado`);
   }
+});
+
+
+router.get('/crearFlashcard', (req, res) => {
+  res.render('crearFlashcard');
+});
+
+
+router.get('/edit/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const pregunta = preguntasService.getPreguntaById(id);
+  res.status(302).render('editarFlashcard', pregunta);
+});
+
+router.post('/actualizarPregunta', (req, res) => {
+  const {id, pregunta, respuesta, imagenPregunta, imagenRespuesta, tema, dificultad, comentarios } = req.body;
+
+  // Llamar al servicio de preguntas para agregar la pregunta
+  const actualizarPregunta = preguntasService.actualizarPregunta(id,pregunta, respuesta, imagenPregunta, imagenRespuesta, tema, dificultad, comentarios );
+  if(actualizarPregunta){
+    res.redirect("/")
+  } else{
+    res.status(404).redirect(`/error/nuevaPregunta/No se pudo agregar la nueva pregunta`)  }
+  // Puedes redirigir a la página de detalles o hacer algo más aquí
+  // res.redirect(`/detalle/${newPregunta.id}`); 
 });
 
 export default router;
--- src/preguntasService.js
+++ src/preguntasService.js
@@ -1,20 +1,20 @@
 // preguntasService.js
 let preguntas = [
-  { id: 1, pregunta: "¿Cuanto es 2+2?", respuesta: "La respuesta es 4", tema: "3", dificultad: "baja", comentarios: []},
-  { id: 2, pregunta: "¿Quien es el CEO de SpaceX?", respuesta: "El CEO de SpaceX es Elon Musk", tema: "6", dificultad: "alta"},
-  { id: 3, pregunta: "¿Cuanto es 2*2?", respuesta: "La respuesta es 4", tema: "9", dificultad: "media"},
-  { id: 4, pregunta: "¿Cuál es la capital de Francia?", respuesta: "La capital de Francia es París", tema: "7", dificultad: "media"},
-  { id: 5, pregunta: "¿En qué año se fundó la ONU?", respuesta: "La ONU fue fundada en 1945", tema: "8", dificultad: "alta"},
-  { id: 6, pregunta: "¿Cuál es el símbolo químico del oro?", respuesta: "El símbolo químico del oro es Au", tema: "5", dificultad: "media"},
-  { id: 7, pregunta: "¿Quién escribió 'Cien años de soledad'?", respuesta: "Gabriel García Márquez escribió 'Cien años de soledad'", tema: "4", dificultad: "alta"},
-  { id: 8, pregunta: "¿Cuál es el río más largo del mundo?", respuesta: "El río más largo del mundo es el río Amazonas", tema: "2", dificultad: "alta"},
-  { id: 9, pregunta: "¿En qué año llegó el hombre a la luna por primera vez?", respuesta: "El hombre llegó a la luna por primera vez en 1969", tema: "6", dificultad: "alta"},
-  { id: 10, pregunta: "¿Cuál es la fórmula química del agua?", respuesta: "La fórmula química del agua es H2O", tema: "5", dificultad: "baja"},
-  { id: 11, pregunta: "¿Quién pintó la Mona Lisa?", respuesta: "La Mona Lisa fue pintada por Leonardo da Vinci", tema: "4", dificultad: "alta"},
-  { id: 12, pregunta: "¿Cuál es la montaña más alta del mundo?", respuesta: "La montaña más alta del mundo es el Monte Everest", tema: "2", dificultad: "alta"},
-  { id: 13, pregunta: "¿Qué planeta es conocido como el planeta rojo?", respuesta: "Marte es conocido como el planeta rojo", tema: "9", dificultad: "media"},
-  { id: 14, pregunta: "¿En qué año comenzó la Segunda Guerra Mundial?", respuesta: "La Segunda Guerra Mundial comenzó en 1939", tema: "8", dificultad: "alta"},
-  { id: 15, pregunta: "¿Cuál es el órgano más grande del cuerpo humano?", respuesta: "La piel es el órgano más grande del cuerpo humano", tema: "1", dificultad: "media"}
+  { id: 1, imagen_pregunta_url: "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-달에-앉아-우주-비행사의-만화-이미지-고품질-일러스트레이션.jpg", imagen_respuesta_url: "https://formacionparaformadores.com/wp-content/uploads/2015/01/Ser-Formador-preguntas-y-re-630x315.jpg", pregunta: "¿Cuanto es 2+2?", respuesta: "La respuesta es 4", tema: "3", dificultad: "baja", comentarios: ["Prueba1", "Prueba2"] },
+  { id: 2, imagen_pregunta_url: "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-달에-앉아-우주-비행사의-만화-이미지-고품질-일러스트레이션.jpg", imagen_respuesta_url: "https://formacionparaformadores.com/wp-content/uploads/2015/01/Ser-Formador-preguntas-y-re-630x315.jpg", pregunta: "¿Quien es el CEO de SpaceX?", respuesta: "El CEO de SpaceX es Elon Musk", tema: "6", dificultad: "alta", comentarios: [] },
+  { id: 3, imagen_pregunta_url: "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-달에-앉아-우주-비행사의-만화-이미지-고품질-일러스트레이션.jpg", imagen_respuesta_url: "https://formacionparaformadores.com/wp-content/uploads/2015/01/Ser-Formador-preguntas-y-re-630x315.jpg", pregunta: "¿Cuanto es 2*2?", respuesta: "La respuesta es 4", tema: "9", dificultad: "media", comentarios: [] },
+  { id: 4, imagen_pregunta_url: "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-달에-앉아-우주-비행사의-만화-이미지-고품질-일러스트레이션.jpg", imagen_respuesta_url: "https://formacionparaformadores.com/wp-content/uploads/2015/01/Ser-Formador-preguntas-y-re-630x315.jpg", pregunta: "¿Cuál es la capital de Francia?", respuesta: "La capital de Francia es París", tema: "7", dificultad: "media", comentarios: [] },
+  { id: 5, imagen_pregunta_url: "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-달에-앉아-우주-비행사의-만화-이미지-고품질-일러스트레이션.jpg", imagen_respuesta_url: "https://formacionparaformadores.com/wp-content/uploads/2015/01/Ser-Formador-preguntas-y-re-630x315.jpg", pregunta: "¿En qué año se fundó la ONU?", respuesta: "La ONU fue fundada en 1945", tema: "8", dificultad: "alta", comentarios: [] },
+  { id: 6, imagen_pregunta_url: "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-달에-앉아-우주-비행사의-만화-이미지-고품질-일러스트레이션.jpg", imagen_respuesta_url: "https://formacionparaformadores.com/wp-content/uploads/2015/01/Ser-Formador-preguntas-y-re-630x315.jpg", pregunta: "¿Cuál es el símbolo químico del oro?", respuesta: "El símbolo químico del oro es Au", tema: "5", dificultad: "media", comentarios: [] },
+  { id: 7, imagen_pregunta_url: "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-달에-앉아-우주-비행사의-만화-이미지-고품질-일러스트레이션.jpg", imagen_respuesta_url: "https://formacionparaformadores.com/wp-content/uploads/2015/01/Ser-Formador-preguntas-y-re-630x315.jpg", pregunta: "¿Quién escribió 'Cien años de soledad'?", respuesta: "Gabriel García Márquez escribió 'Cien años de soledad'", tema: "4", dificultad: "alta", comentarios: [] },
+  { id: 8, imagen_pregunta_url: "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-달에-앉아-우주-비행사의-만화-이미지-고품질-일러스트레이션.jpg", imagen_respuesta_url: "https://formacionparaformadores.com/wp-content/uploads/2015/01/Ser-Formador-preguntas-y-re-630x315.jpg", pregunta: "¿Cuál es el río más largo del mundo?", respuesta: "El río más largo del mundo es el río Amazonas", tema: "2", dificultad: "alta", comentarios: [] },
+  { id: 9, imagen_pregunta_url: "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-달에-앉아-우주-비행사의-만화-이미지-고품질-일러스트레이션.jpg", imagen_respuesta_url: "https://formacionparaformadores.com/wp-content/uploads/2015/01/Ser-Formador-preguntas-y-re-630x315.jpg", pregunta: "¿En qué año llegó el hombre a la luna por primera vez?", respuesta: "El hombre llegó a la luna por primera vez en 1969", tema: "6", dificultad: "alta", comentarios: [] },
+  { id: 10, imagen_pregunta_url: "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-달에-앉아-우주-비행사의-만화-이미지-고품질-일러스트레이션.jpg", imagen_respuesta_url: "https://formacionparaformadores.com/wp-content/uploads/2015/01/Ser-Formador-preguntas-y-re-630x315.jpg", pregunta: "¿Cuál es la fórmula química del agua?", respuesta: "La fórmula química del agua es H2O", tema: "5", dificultad: "baja", comentarios: [] },
+  { id: 11, imagen_pregunta_url: "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-달에-앉아-우주-비행사의-만화-이미지-고품질-일러스트레이션.jpg", imagen_respuesta_url: "https://formacionparaformadores.com/wp-content/uploads/2015/01/Ser-Formador-preguntas-y-re-630x315.jpg", pregunta: "¿Quién pintó la Mona Lisa?", respuesta: "La Mona Lisa fue pintada por Leonardo da Vinci", tema: "4", dificultad: "alta", comentarios: [] },
+  { id: 12, imagen_pregunta_url: "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-달에-앉아-우주-비행사의-만화-이미지-고품질-일러스트레이션.jpg", imagen_respuesta_url: "https://formacionparaformadores.com/wp-content/uploads/2015/01/Ser-Formador-preguntas-y-re-630x315.jpg", pregunta: "¿Cuál es la montaña más alta del mundo?", respuesta: "La montaña más alta del mundo es el Monte Everest", tema: "2", dificultad: "alta", comentarios: [] },
+  { id: 13, imagen_pregunta_url: "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-달에-앉아-우주-비행사의-만화-이미지-고품질-일러스트레이션.jpg", imagen_respuesta_url: "https://formacionparaformadores.com/wp-content/uploads/2015/01/Ser-Formador-preguntas-y-re-630x315.jpg", pregunta: "¿Qué planeta es conocido como el planeta rojo?", respuesta: "Marte es conocido como el planeta rojo", tema: "9", dificultad: "media", comentarios: [] },
+  { id: 14, imagen_pregunta_url: "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-달에-앉아-우주-비행사의-만화-이미지-고품질-일러스트레이션.jpg", imagen_respuesta_url: "https://formacionparaformadores.com/wp-content/uploads/2015/01/Ser-Formador-preguntas-y-re-630x315.jpg", pregunta: "¿En qué año comenzó la Segunda Guerra Mundial?", respuesta: "La Segunda Guerra Mundial comenzó en 1939", tema: "8", dificultad: "alta", comentarios: [] },
+  { id: 15, imagen_pregunta_url: "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-달에-앉아-우주-비행사의-만화-이미지-고품질-일러스트레이션.jpg", imagen_respuesta_url: "https://formacionparaformadores.com/wp-content/uploads/2015/01/Ser-Formador-preguntas-y-re-630x315.jpg", pregunta: "¿Cuál es el órgano más grande del cuerpo humano?", respuesta: "La piel es el órgano más grande del cuerpo humano", tema: "1", dificultad: "media", comentarios: [] }
 ];
 
 const preguntasService = {
@@ -26,11 +26,11 @@ const preguntasService = {
     return preguntas.find((pregunta) => pregunta.id === id);
   },
 
-  addPregunta: (pregunta, respuesta) => {
+  addPregunta: (imagen_pregunta_url, imagen_respuesta_url, pregunta, respuesta, tema, dificultad, comentarios) => {
     const newId = preguntas.length + 1;
-    const newPregunta = { id: newId, pregunta, respuesta };
+    const newPregunta = { id: newId, imagen_pregunta_url, imagen_respuesta_url, pregunta, respuesta, tema, dificultad, comentarios };
     preguntas.push(newPregunta);
-    return newPregunta;
+    return preguntas;
   },
 
   updatePregunta: (id, pregunta, respuesta) => {
@@ -51,6 +51,22 @@ const preguntasService = {
     }
     return null;
   },
+
+  actualizarPregunta: (id, imagen_pregunta_url, imagen_respuesta_url, pregunta, respuesta, dificultad) => {
+    const index = preguntas.findIndex((p) => p.id === id);
+    alert(index)
+    if (index !== -1) {
+      preguntas[index].pregunta = pregunta;
+      preguntas[index].respuesta = respuesta;
+      preguntas[index].imagen_pregunta_url = imagen_pregunta_url;
+      preguntas[index].imagen_respuesta_url = imagen_respuesta_url;
+      preguntas[index].dificultad = dificultad;
+      return preguntas[index];
+    }
+    return null;
+  },
+ 
+
 };
 
 export default preguntasService;
